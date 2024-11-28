9ae41fbf72a7995751223aba1c57b774543a02dd
pawelglaba <64959534+pawelglaba@users.noreply.github.com>
2024-11-28 21:11:48 +0100

    Firebase Auth Lecture

---
+++ app/src/main/java/com/example/firebaseauthdemo/firebase/User.kt
@@ -0,0 +1,7 @@
+package com.example.firebaseauthdemo.firebase
+
+class User( val id: String="",
+            val name: String="",
+            val registeredUser: Boolean = false,
+            val email: String="",
+)
